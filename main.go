21b6beac2ab6413461b79ba1c23689072612d8d3
Zachary Kent <ztkent@gmail.com>
2024-05-10 09:06:48 -0400

    dashboard setup

--- main.go
+++ main.go
@@ -4,41 +4,12 @@ import (
 	"os"
 
 	"github.com/Ztkent/go-nps"
+	"github.com/Ztkent/nps-dashboard/internal/dashboard"
 )
 
 func main() {
 	npsApi := nps.NewNpsApi(os.Getenv("NPS_API_KEY"))
-	ListImportantPeopleInPark(npsApi, "ACAD")
-	GetVisitorCentersInState(npsApi, "CA")
-	ListAllParks(npsApi)
-}
-
-func ListAllParks(npsApi nps.NpsApi) {
-	res, err := npsApi.GetParks(nil, nil, 0, 0, "", nil)
-	if err != nil {
-		panic(err)
-	}
-	for _, park := range res.Data {
-		println(park.Name)
-	}
-}
-
-func GetVisitorCentersInState(npsApi nps.NpsApi, state string) {
-	res, err := npsApi.GetVisitorCenters(nil, []string{state}, "", 0, 1, nil)
-	if err != nil {
-		panic(err)
-	}
-	for _, visitorCenter := range res.Data {
-		println(visitorCenter.Name)
-	}
-}
-
-func ListImportantPeopleInPark(npsApi nps.NpsApi, siteCode string) {
-	res, err := npsApi.GetPeople([]string{siteCode}, nil, "", 0, 0)
-	if err != nil {
-		panic(err)
-	}
-	for _, person := range res.Data {
-		println(person.FirstName + " " + person.LastName)
-	}
+	dashboard.ListImportantPeopleInPark(npsApi, "ACAD")
+	dashboard.GetVisitorCentersInState(npsApi, "CA")
+	dashboard.ListAllParks(npsApi)
 }
